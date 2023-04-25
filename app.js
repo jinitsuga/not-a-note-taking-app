1ade890bbe906ea83dea71d12968441658694a33
agu <iba.agustin@gmail.com>
2023-04-25 11:02:08 -0300

    added logger and config on main app, configured mongo db connection

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const notesRouter = require("./controllers/notes");
-
+const logger = require("./utils/logger");
+const config = require("./utils/config");
 const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
+
+mongoose
+  .connect(config.MONGO_URL)
+  .then(() => {
+    logger.info("conected to MongoDB");
+  })
+  .catch((err) => {
+    logger.error("error connecting to DB:", err.message);
+  });
 
 app.use(cors());
 app.use(express.json());
