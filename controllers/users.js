1b4507bcbfa0afa1c1de490ae79693df8ddd0a76
agu <iba.agustin@gmail.com>
2023-04-29 23:10:16 -0300

    added router for users. Only posting users to db for now

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const usersRouter = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const { response } = require("../app");
+
+usersRouter.post("/", async (req, res) => {
+  const { username, name, password } = req.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username: username,
+    name: name,
+    passwordHash: passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
