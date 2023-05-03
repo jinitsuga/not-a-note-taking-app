ebee577f8e1f61130b3b28698a98f7e0fe2310c8
agu <iba.agustin@gmail.com>
2023-05-03 09:48:50 -0300

    modified note creation to include logged in user's id

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,6 +2,7 @@ const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const User = require("../models/user");
 const getTokenFrom = require("../utils/list_helper").getTokenFrom;
+const jwt = require("jsonwebtoken");
 
 notesRouter.get("/", async (req, res, next) => {
   const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
@@ -16,7 +17,12 @@ notesRouter.get("/:id", async (req, res, next) => {
 notesRouter.post("/", async (req, res, next) => {
   const data = req.body;
 
-  const user = await User.findById(data.userId);
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: "invalid token" });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   console.log(user);
 
