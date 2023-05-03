81fb5c7462e8cbe22b4b8d182b7649a31f746aa6
agu <iba.agustin@gmail.com>
2023-05-02 21:09:43 -0300

    added fetching entire notes along with user, instead of only notes ids

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,7 @@ usersRouter.post("/", async (req, res) => {
 });
 
 usersRouter.get("/", async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("notes");
 
   res.json(users);
 });
